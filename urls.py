d6532b6ae7c992ac7c96800d05b3ff2a5e0ebcd1
Robert Kleinsasser <robert.kleinsasser@gmail.com>
2012-09-04 16:23:09 -0600

    Switched to generic views

--- urls.py
+++ urls.py
@@ -1,6 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import ListView, DetailView
+from brain_dump.models import Dump
 
-urlpatterns = patterns('brain_dump.views',
-    url(r'^$', 'index'),
-    url(r'^(?P<dump_id>\d+)/$', 'detail'),
+urlpatterns = patterns('',
+    url(r'^$',
+      ListView.as_view(
+        queryset=Dump.objects.order_by('-date'),
+        context_object_name='dump_list',
+        template_name='dumps/index.html'),
+      name='dump_index'), 
+    url(r'^(?P<pk>\d+)/$',
+      DetailView.as_view(
+        model=Dump,
+        template_name='dumps/detail.html'),
+      name='dump_detail'),
 )
--- views.py
+++ views.py
@@ -2,10 +2,3 @@
 from brain_dump.models import Dump
 from django.shortcuts import render_to_response, get_object_or_404
 
-def index(request):
-  dump_list = Dump.objects.all().order_by('date')
-  return render_to_response('dumps/index.html', {'dump_list': dump_list})
-
-def detail(request, dump_id):
-  dump = get_object_or_404(Dump, pk=dump_id)
-  return render_to_response('dumps/detail.html', {'dump': dump})
