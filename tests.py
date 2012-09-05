bed0c530cf07ecd2da92a70629e25278f7a82737
Robert Kleinsasser <robert.kleinsasser@gmail.com>
2012-09-05 10:58:11 -0600

    Added unit tests for Link model

--- tests.py
+++ tests.py
@@ -6,11 +6,36 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from brain_dump.models import Dump, Link
 
+class LinkTestCase(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+  def test_creation(self):
+    my_url = 'http://awesome.url.com'
+    link = Link(url=my_url)
+    self.assertEqual(my_url, link.url) 
+
+  def test_is_a_picture(self):
+    PICTURES = [
+      'test.jpeg',
+      'hello.jpg',
+      'avatar.png',
+      'animated.gif',
+    ]
+      
+    for picture in PICTURES:
+      my_url = "http://localhost/" + picture
+      link = Link(url=my_url)
+      self.assertTrue(link.is_a_picture())
+
+    NOT_PICTURES = [
+      'text.txt',
+      'hello',
+      'goodbye/',
+      'this.is.a.sentence',
+    ]
+    
+    for not_picture in NOT_PICTURES:
+      my_url = "http://localhost/" + not_picture
+      link = Link(url=my_url)
+      self.assertFalse(link.is_a_picture())
