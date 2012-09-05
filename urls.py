84e9680ce20d0a48471046a670958ecfae4bf0f6
Robert Kleinsasser <robert.kleinsasser@gmail.com>
2012-09-04 21:48:16 -0600

    Add authentication indicator to dump index

--- urls.py
+++ urls.py
@@ -2,9 +2,16 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 from brain_dump.models import Dump
 
+class RequestListView(ListView):
+  def get_context_data(self, **kwargs):
+    #grab the context
+    context = super(RequestListView, self).get_context_data(**kwargs)
+    context.update({ 'request': self.request })
+    return context
+
 urlpatterns = patterns('',
     url(r'^$',
-      ListView.as_view(
+      RequestListView.as_view(
         queryset=Dump.objects.order_by('-date'),
         context_object_name='dump_list',
         template_name='dumps/index.html'),
--- views.py
+++ views.py
@@ -2,3 +2,5 @@
 from brain_dump.models import Dump
 from django.shortcuts import render_to_response, get_object_or_404
 
+from django.http import HttpResponse
+
