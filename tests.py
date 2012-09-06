3cc932cabcae7e2f066a763491098cfb72f112e9
Robert Kleinsasser <robert.kleinsasser@gmail.com>
2012-09-05 21:17:39 -0600

    Added unit tests for Dump and Dump/Link relation

--- tests.py
+++ tests.py
@@ -7,6 +7,24 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from brain_dump.models import Dump, Link
+from django.db import DatabaseError
+
+class DumpTestCase(TestCase):
+  
+  def dump_creation(self):
+    my_type = 'L'
+    my_title = 'Title'
+    my_description = 'Description'
+    my_follow_up = True
+    my_date = timezone.now()
+
+    dump = Dump(type=my_type, title=my_title, description=my_description, follow_up=my_follow_up, date=my_date)
+
+    self.assertEqual(my_type, dump.type)
+    self.assertEqual(my_title, dump.title)
+    self.assertEqual(my_description, dump.description)
+    self.assertEqual(my_follow_up, dump.follow_up)
+    self.assertEqual(my_date, dump.date)
 
 class LinkTestCase(TestCase):
 
@@ -58,3 +76,16 @@ class LinkTestCase(TestCase):
       my_url = "http://localhost/" + not_picture
       link = Link(url=my_url)
       self.assertFalse(link.is_a_picture())
+
+class DumpWithLinksTestCase(TestCase):
+
+  def test_link_set_in_dump(self):
+    my_title = 'Simple Dump'
+    dump = Dump(title=my_title)
+    dump.save()
+    link = dump.link_set.create(url='http://localhost')
+    link.save()
+
+    self.assertIsInstance(link, Link)
+    self.assertEqual(dump.link_set.all()[0], link)
+    self.assertEqual(link.dump.title, my_title)
