be940e40d5c159b9dd114bd6701bb17a9009fc26
Robert Kleinsasser <robert.kleinsasser@gmail.com>
2012-09-06 09:36:48 -0600

    Added unit tests for the list and detail views

--- tests.py
+++ tests.py
@@ -8,8 +8,10 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 from brain_dump.models import Dump, Link
 from django.db import DatabaseError
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
-class DumpTestCase(TestCase):
+class ModelDumpTestCase(TestCase):
   
   def dump_creation(self):
     my_type = 'L'
@@ -26,7 +28,7 @@ class DumpTestCase(TestCase):
     self.assertEqual(my_follow_up, dump.follow_up)
     self.assertEqual(my_date, dump.date)
 
-class LinkTestCase(TestCase):
+class ModelLinkTestCase(TestCase):
 
   def test_creation(self):
     my_url = 'http://awesome.url.com'
@@ -77,7 +79,7 @@ class LinkTestCase(TestCase):
       link = Link(url=my_url)
       self.assertFalse(link.is_a_picture())
 
-class DumpWithLinksTestCase(TestCase):
+class ModelDumpWithLinksTestCase(TestCase):
 
   def test_link_set_in_dump(self):
     my_title = 'Simple Dump'
@@ -89,3 +91,41 @@ class DumpWithLinksTestCase(TestCase):
     self.assertIsInstance(link, Link)
     self.assertEqual(dump.link_set.all()[0], link)
     self.assertEqual(link.dump.title, my_title)
+
+class ViewDumpsListTestCase(TestCase):
+
+  def test_response(self):
+    response = self.client.get('/dumps/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_logged_out(self):
+    response = self.client.get('/dumps/')
+    found = response.content.find('Logged out') > -1
+    self.assertTrue(found)
+
+  def test_logged_in(self):
+    User.objects.create_user('john', 'john@mail.com', 'password')
+    
+    user = self.client.login(username='john', password='password')
+    response = self.client.get('/dumps/')
+    found = response.content.find('Logged in: john') > -1
+
+    self.assertTrue(found)
+
+  def test_adding_to_list(self):
+    response = self.client.get('/dumps/')
+    self.assertTrue(response.content.find('Brain Dump') > -1)
+    self.assertFalse(response.content.find('My Test Title') > -1)
+    
+    Dump.objects.create(title='My Test Title')
+    response = self.client.get('/dumps/')
+    self.assertTrue(response.content.find('My Test Title') > -1)
+
+class ViewDumpsDetailTestCase(TestCase):
+
+  def test_response(self):
+    dump = Dump(title='Title')
+    dump.save()
+    response = self.client.get('/dumps/%d/' % dump.id)
+    self.assertEqual(response.status_code, 200)
+
