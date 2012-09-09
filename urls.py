5fa052550012fc9128cc67ac77fd6bb92d71b5ed
Robert Kleinsasser <robert.kleinsasser@gmail.com>
2012-09-09 10:20:38 -0600

    Added template inheritance to clean up view. Removed unnecessary passing of request to template.

--- urls.py
+++ urls.py
@@ -2,16 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 from brain_dump.models import Dump
 
-class RequestListView(ListView):
-  def get_context_data(self, **kwargs):
-    #grab the context
-    context = super(RequestListView, self).get_context_data(**kwargs)
-    context.update({ 'request': self.request })
-    return context
-
 urlpatterns = patterns('',
     url(r'^$',
-      RequestListView.as_view(
+      ListView.as_view(
         queryset=Dump.objects.order_by('-date'),
         context_object_name='dump_list',
         template_name='dumps/index.html'),
